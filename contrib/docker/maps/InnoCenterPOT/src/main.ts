deaab49f970d50d08b5fa67c31ec835e3d6f7098
Leo310 <leodev310@gmail.com>
2022-07-15 15:40:37 +0200

    Add: final updates

--- contrib/docker/maps/InnoCenterPOT/src/main.ts
+++ contrib/docker/maps/InnoCenterPOT/src/main.ts
@@ -1,33 +1,10 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
-import axios from "axios";
 
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
-
-function getData() {
-  const encodedParams = new URLSearchParams();
-  encodedParams.append("length", "12");
-  encodedParams.append("type", "numeric");
-  console.log("hello");
-
-  const options = {
-    method : 'POST',
-    url : 'https://random-string-generator.p.rapidapi.com/randomstring',
-    params : {length : '12', type : 'numeric'},
-    headers : {
-      'content-type' : 'application/x-www-form-urlencoded',
-      'X-RapidAPI-Key' : 'SIGN-UP-FOR-KEY',
-      'X-RapidAPI-Host' : 'random-string-generator.p.rapidapi.com'
-    },
-    data : encodedParams
-  };
-  axios.request(options)
-      .then(function(response) { console.log("DATAAAA: ", response.data); })
-      .catch(function(error) { console.error(error); });
-}
 
 // Waiting for the API to be ready
 WA.onInit()
@@ -35,14 +12,127 @@ WA.onInit()
       console.log('Scripting API ready');
       // getData();
       console.log('Player tags: ', WA.player.tags)
+      WA.room.hideLayer('Eingang Tür auf');
+      WA.room.hideLayer('Eingang2 Tür auf');
 
-      WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+      WA.room.onEnterLayer('Eingang Tür Zone').subscribe(() => {
+        if (WA.player.state.access as Number >= 1) {
+
+          WA.room.hideLayer('Eingang Tür zu');
+          WA.room.showLayer('Eingang Tür auf');
+        } else {
+
+          currentPopup = WA.ui.openPopup('Eingang Popup', "No Access!", []);
+        }
       })
 
-      WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+      WA.room.onLeaveLayer('Eingang Tür Zone').subscribe(() => {
+        closePopUp();
+
+        WA.room.showLayer('Eingang Tür zu');
+        WA.room.hideLayer('Eingang Tür auf');
+      })
+
+      WA.room.onEnterLayer('Eingang2 Tür Zone').subscribe(() => {
+        if (WA.player.state.access as Number >= 1) {
+
+          WA.room.hideLayer('Eingang2 Tür zu');
+          WA.room.showLayer('Eingang2 Tür auf');
+        } else {
+
+          currentPopup = WA.ui.openPopup('Eingang2 Popup', "No Access!", []);
+        }
+      })
+
+      WA.room.onLeaveLayer('Eingang2 Tür Zone').subscribe(() => {
+        closePopUp();
+
+        WA.room.hideLayer('Eingang2 Tür auf');
+        WA.room.showLayer('Eingang2 Tür zu');
+      })
+
+      WA.chat.onChatMessage((mes) => {
+        var zone = WA.player.state.chatZone;
+
+        console.log(zone);
+        console.log(mes);
+
+        if (zone == "") {
+          return;
+        } else if (zone == "kitchen") {
+
+          if (mes.toLowerCase() == "hummer") {
+            WA.chat.sendChatMessage("Guten Appetit!", "Mitarbeiter");
+          } else if (mes.toLocaleLowerCase() == "hühnchen") {
+            WA.chat.sendChatMessage("Guten Appetit!", "Mitarbeiter");
+          } else if (mes.toLocaleLowerCase() == "nudeln") {
+            WA.chat.sendChatMessage("Guten Appetit!", "Mitarbeiter");
+          } else {
+            WA.chat.sendChatMessage(
+                "Das habe ich leider nicht verstanden...Kannst du das wiederholen?",
+                "Mitarbeiter")
+          }
+        } else if (zone == "secretary") {
+
+          if (mes.toLowerCase() == "daily badge") {
+
+            WA.chat.sendChatMessage(
+                'Wilst du eine Badge abgeben oder ausleihen?', "Sekretärin");
+
+          } else if (mes.toLocaleLowerCase() == "auskunft") {
+            WA.chat.sendChatMessage(
+                "Wenn du Hilfe brauchst guck dir am besten das Info Video an. Bei weiteren Fragen komm gerne zu mir",
+                "Sekretärin");
+          } else if (mes.toLocaleLowerCase() == "covid test") {
+            WA.chat.sendChatMessage("Dein Test ist negativ. Bleib gesund!",
+                                    "Sekretär*in");
+          } else if (mes.toLocaleLowerCase() == "abgeben") {
+
+            WA.player.state.access = 0;
+            WA.chat.sendChatMessage(
+                'Dankeschön. Du hast nun kein Zutritt mehr. Vergiss morgen deine Badge nicht',
+                "Sekretärin");
+          } else if (mes.toLocaleLowerCase() == "ausleihen") {
+
+            WA.player.state.access = 1;
+            WA.chat.sendChatMessage(
+                "Hier ist dein Daily Badge. Du hast nun Zutritt zum Gebäude." +
+                    "Vergiss nicht die Badge am Ende des Tages wieder abzugeben!",
+                "Sekretärin");
+          } else {
+            WA.chat.sendChatMessage(
+                "Das habe ich leider nicht verstanden...Kannst du das wiederholen?",
+                "Mitarbeiter")
+          }
+        }
+      })
+      WA.room.onEnterLayer('Secretary Zone').subscribe(() => {
+        WA.player.state.chatZone = "secretary";
+
+        console.log(WA.player.state.chatZone);
+        WA.chat.sendChatMessage(
+            'Hallo wie kann ich dir helfen? Ich kann dir eine Auskunft geben, dir eine Daily Badge ausleihen oder einen Covid Test machen.',
+            "Sekretaer*in");
+      })
+      WA.room.onEnterLayer('Kitchen Zone').subscribe(() => {
+        WA.player.state.chatZone = "kitchen"
+        WA.chat.sendChatMessage(
+            'Hier ist die Cafeteria. Wollen Sie Hummer, Hühnchen oder Nudeln?',
+            "Mitarbeiter");
+      })
+
+      WA.room.onLeaveLayer('Kitchen Zone')
+          .subscribe(() => { WA.player.state.chatZone = ""; })
+      WA.room.onLeaveLayer('Secretary Zone')
+          .subscribe(() => { WA.player.state.chatZone = ""; })
+
+      // WA.room.onEnterLayer('clockZone').subscribe(() => {
+      //   const today = new Date();
+      //   const time = today.getHours() + ":" + today.getMinutes();
+      //   currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+      // })
+
+      // WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
 
       // The line below bootstraps the Scripting API Extra library that adds a
       // number of advanced properties/features to WorkAdventure
--- pusher/src/Enum/EnvironmentVariable.ts
+++ pusher/src/Enum/EnvironmentVariable.ts
@@ -1,4 +1,4 @@
-const SECRET_KEY = process.env.SECRET_KEY || "THECODINGMACHINE_SECRET_KEY";
+const SECRET_KEY = process.env.SECRET_KEY || "yourSecretKey";
 const ALLOW_ARTILLERY = process.env.ALLOW_ARTILLERY ? process.env.ALLOW_ARTILLERY == "true" : false;
 const API_URL = process.env.API_URL || "";
 const ADMIN_API_URL = process.env.ADMIN_API_URL || "";
@@ -9,7 +9,7 @@ const CPU_OVERHEAT_THRESHOLD = Number(process.env.CPU_OVERHEAT_THRESHOLD) || 80;
 const PUSHER_HTTP_PORT = parseInt(process.env.PUSHER_HTTP_PORT || "8080") || 8080;
 export const SOCKET_IDLE_TIMER = parseInt(process.env.SOCKET_IDLE_TIMER as string) || 120; // maximum time (in second) without activity before a socket is closed. Should be greater than 60 seconds in order to cope for Chrome intensive throttling (https://developer.chrome.com/blog/timer-throttling-in-chrome-88/#intensive-throttling)
 
-export const FRONT_URL = process.env.FRONT_URL || "http://localhost";
+export const FRONT_URL = process.env.FRONT_URL || "https://play.herginnies.world";
 export const OPID_CLIENT_ID = process.env.OPID_CLIENT_ID || "";
 export const OPID_CLIENT_SECRET = process.env.OPID_CLIENT_SECRET || "";
 export const OPID_CLIENT_ISSUER = process.env.OPID_CLIENT_ISSUER || "";
@@ -20,15 +20,15 @@ export const OPID_USERNAME_CLAIM = process.env.OPID_USERNAME_CLAIM || "username"
 export const OPID_LOCALE_CLAIM = process.env.OPID_LOCALE_CLAIM || "locale";
 export const DISABLE_ANONYMOUS: boolean = process.env.DISABLE_ANONYMOUS === "true";
 export const PROMETHEUS_AUTHORIZATION_TOKEN = process.env.PROMETHEUS_AUTHORIZATION_TOKEN;
-export const EJABBERD_DOMAIN: string = process.env.EJABBERD_DOMAIN || "";
-export const EJABBERD_URI: string = process.env.EJABBERD_URI || "";
-export const EJABBERD_JWT_SECRET: string = process.env.EJABBERD_JWT_SECRET || "";
+export const EJABBERD_DOMAIN: string = process.env.EJABBERD_DOMAIN || "ejabberd";
+export const EJABBERD_URI: string = process.env.EJABBERD_URI || "ejabberd:5380";
+export const EJABBERD_JWT_SECRET: string = process.env.EJABBERD_JWT_SECRET || "yourSecretKey";
 
 // If set to the string "true", the /openapi route will return the OpenAPI definition and the swagger-ui/ route will display the documentation
 export const ENABLE_OPENAPI_ENDPOINT = process.env.ENABLE_OPENAPI_ENDPOINT === "true";
 
 // The URL to use if the user is visiting the first time and hitting the "/" route.
-export const START_ROOM_URL: string = process.env.START_ROOM_URL || "/_/global/maps.workadventu.re/starter/map.json";
+export const START_ROOM_URL: string = process.env.START_ROOM_URL || "/_/global/maps.herginnies.world/InnoCenterPOT/map.json";
 
 export {
     SECRET_KEY,
